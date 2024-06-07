f8f2fc909e1139eb5ee5688efdd01391446e0fe2
Pedro Foltram <pedroaugustofolb@gmail.com>
2024-06-07 09:08:32 -0300

    add: new image to login page

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import WorkersImage from "@/assets/workers.svg";
+import Workers2Image from "@/assets/workers2.svg";
 
 export default function LoginLayout({
   children,
@@ -10,7 +10,7 @@ export default function LoginLayout({
     <>
       <section className="flex h-full flex-col items-center justify-center z-10 w-full lg:max-w-[700px]">{children}</section>
       <div className="hidden h-full items-center justify-center lg:flex">
-        <Image src={WorkersImage} alt="in-meta-software" className="w-[500px] h-[430px]" />
+        <Image src={Workers2Image} alt="in-meta-software" className="w-[500px] h-[430px]" />
       </div>
 
       <aside className="absolute hidden lg:block w-full h-full top-0 z-0 rotate-180 bg-bottom-right-green-gradient" />
