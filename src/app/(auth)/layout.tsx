2cd96396ea0b45b3386fdd81f27707ffad184e15
Pedro Foltram <pedroaugustofolb@gmail.com>
2024-06-06 23:44:02 -0300

    fix: unsed class

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,13 +9,12 @@ export default function AuthLayout({
   return (
     <main className="flex h-screen w-screen justify-center bg-gray-50 gap-8 p-4">
       <section className="flex h-full flex-col items-center justify-center z-10 w-full lg:max-w-[700px]">{children}</section>
-
       <div className="hidden h-full items-center justify-center lg:flex">
         <Image src={WorkersImage} alt="in-meta-software" className="w-[500px] h-[430px]" />
       </div>
 
-      <aside className="absolute hidden lg:block w-full h-full top-0 z-0 to-primary-400 rotate-180 bg-bottom-right-green-gradient" />
-      <aside className="absolute hidden lg:block w-full h-full top-0 z-0 to-primary-400 rotate-180 bg-bottom-right-green-gradient scale-y-[-1]" />
+      <aside className="absolute hidden lg:block w-full h-full top-0 z-0 rotate-180 bg-bottom-right-green-gradient" />
+      <aside className="absolute hidden lg:block w-full h-full top-0 z-0 rotate-180 bg-bottom-right-green-gradient scale-y-[-1]" />
     </main>
   );
 }
